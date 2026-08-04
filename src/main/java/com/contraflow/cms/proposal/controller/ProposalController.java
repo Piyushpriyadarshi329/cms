af79105bb10d4ad26039b022384fa9e5f9dd91a5
piyush <piyush.priyadarshi@antrtepriz.com>
2026-08-04 15:14:54 +0530

    Add Proposal route

--- src/main/java/com/contraflow/cms/proposal/controller/ProposalController.java
+++ src/main/java/com/contraflow/cms/proposal/controller/ProposalController.java
@@ -2,10 +2,7 @@ package com.contraflow.cms.proposal.controller;
 
 
 import com.contraflow.cms.proposal.dto.ProposalRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/propasal")
@@ -14,21 +11,31 @@ public class ProposalController {
 
 
 
-    public String getProposal(@RequestBody ProposalRequest proposalRequest){
-        return "proposal create";
-    }
 
 
-    @PostMapping("/create")
+    @PostMapping("/")
     public String createProposal(@RequestBody ProposalRequest proposalRequest){
         return "proposal create";
     }
-    @PostMapping("/addDiscussion")
-    public String addProposalDiscussion(@RequestBody ProposalRequest proposalRequest){
-        return "proposal create";
+
+
+
+    @PostMapping("/id:discussion")
+    public String createProposalDiscussion(@RequestBody ProposalRequest proposalRequest){
+        return "proposal discussion create";
     }
 
 
+    @GetMapping("/")
+    public String getProposal(){
+        return "fetch proposal";
+    }
+
+
+    @GetMapping("/id:discussion")
+    public String getProposalDiscussion(){
+        return "fetch proposal discussion";
+    }
 
 
 
