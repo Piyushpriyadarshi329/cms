872c5624f829cc0b9c12b3076a093a9bbaeabfd5
Shruti <shruti.solanki@antrepriz.com>
2026-08-04 14:18:50 +0530

    tenant crud controller

--- src/main/java/com/contraflow/cms/tenant/controller/TenantUserController.java
+++ src/main/java/com/contraflow/cms/tenant/controller/TenantUserController.java
@@ -1,11 +1,12 @@
 package com.contraflow.cms.tenant.controller;
 
+import com.contraflow.cms.tenant.dto.TenantUserRequest;
+import com.contraflow.cms.tenant.dto.TenantUserResponse;
 import com.contraflow.cms.tenant.entity.TenantUser;
 import com.contraflow.cms.tenant.services.TenantService;
+import com.contraflow.cms.tenant.services.TenantUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +15,21 @@ import java.util.List;
 public class TenantUserController {
 
     @Autowired
-    private TenantService tenantService;
+    private TenantUserService tenantUserService;
 
+    @GetMapping
+    public List<TenantUser> getAllTenants(){
+        return tenantUserService.getAll();
+    }
 
+    @PostMapping
+    public TenantUserResponse createUser(@RequestBody TenantUserRequest request){
+        return tenantUserService.createUser(request);
+    }
+
+    @PutMapping("/{id}")
+    public TenantUser updateService(@RequestBody TenantUserRequest request,@PathVariable Long id){
+        return tenantUserService.updateUser(request,id);
+    }
 
 }
